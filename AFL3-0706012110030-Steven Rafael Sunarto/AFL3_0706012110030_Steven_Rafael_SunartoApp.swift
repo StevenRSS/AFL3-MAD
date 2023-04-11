3b72c55a71fe698cd792a499a5509c7b51a16067
StevenRSS <ssunarto@student.ciputra.ac.id>
2023-04-11 08:29:54 +0700

    Initial Commit

---
+++ AFL3-0706012110030-Steven Rafael Sunarto/AFL3_0706012110030_Steven_Rafael_SunartoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110030_Steven_Rafael_SunartoApp.swift
+//  AFL3-0706012110030-Steven Rafael Sunarto
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110030_Steven_Rafael_SunartoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
